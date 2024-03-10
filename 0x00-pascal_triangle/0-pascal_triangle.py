945715dc736c11455e709f161747e9d735e1163e
BelalGhonaim <belalpop1999@yahoo.com>
2024-03-10 22:23:35 +0200

    $ git commit -m "0x00. Pascal's Triangle"

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,7 +5,6 @@ that returns a list of lists of integers representing the Pascals triangle of n:
 Returns an empty list if n <= 0
 You can assume n will be always an integer
 """
-
 def pascal_triangle(n):
     i = []
     if n<=0:
