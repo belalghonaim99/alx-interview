c47b36b7b58a1530d31b6ec9cd7e82a1c0a559ca
BelalGhonaim <belalpop1999@yahoo.com>
2024-03-16 03:29:14 +0200

    testing lock

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Lockboxes """
+
+
+def canUnlockAll(boxes):
+    """check if all boxes can be opened"""
+    n = len(boxes)
+    keys = [0]
+    for key in keys:
+        for new_key in boxes[key]:
+            if new_key not in keys and new_key < n:
+                keys.append(new_key)
+    if len(keys) == n:
+        return True
+    return False
+    
