be35e75d3c1e731a9e649398b4e1d28b7491e6c9
BelalGhonaim <belalpop1999@yahoo.com>
2024-03-23 19:20:35 +0200

    add file readme and 0 mian and 0mino

---
+++ 0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Min Operations """
+
+
+def minOperations(n):
+    """ Min Operations """
+    re = 'H'
+    re2 = 'H'
+    opeartion = 0
+    while (len(re2) < n):
+        if (n % len(re2) == 0):
+            re = re2
+            re2 += re2
+        re2 += re
+        re2 += re2
+    else:
+        opeartion += 1
+        re2 += re
+    if (len(re2) != n):
+        return 0
+    return opeartion
