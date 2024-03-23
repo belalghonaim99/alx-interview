22f439d37cf4ee03c4d77be0eaae80b17aafd49b
BelalGhonaim <belalpop1999@yahoo.com>
2024-03-23 19:26:10 +0200

    removes 2 line dublicate

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,11 +9,9 @@ def minOperations(n: int) -> int:
     opeartion = 0
     while (len(re2) < n):
         if (n % len(re2) == 0):
-            opeartion += 1
+            opeartion += 2
             re = re2
             re2 += re2
-        re2 += re
-        re2 += re2
     else:
         opeartion += 1
         re2 += re
