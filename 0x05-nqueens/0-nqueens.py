28ba0ef81eb69da1c452532afb38062490999a7c
BelalGhonaim <belalpop1999@yahoo.com>
2024-04-13 02:43:32 +0200

    add file 0 and file

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+""" N-queens problem. """
+import sys
+
+
+def is_safe(board, row, col):
+    # Check if there is a queen in the same column
+    for i in range(row):
+        if board[i][col] == 'Q':
+            return False
+    
+    # Check if there is a queen in the upper left diagonal
+    i = row - 1
+    j = col - 1
+    while i >= 0 and j >= 0:
+        if board[i][j] == 'Q':
+            return False
+        i -= 1
+        j -= 1
+    
+    # Check if there is a queen in the upper right diagonal
+    i = row - 1
+    j = col + 1
+    while i >= 0 and j < len(board):
+        if board[i][j] == 'Q':
+            return False
+        i -= 1
+        j += 1
+    
+    return True
+
+
+def solve_nqueens(n):
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    
+    board = [['.' for _ in range(n)] for _ in range(n)]
+    solutions = []
+    
+    def backtrack(row):
+        if row == n:
+            solutions.append([''.join(row) for row in board])
+            return
+        
+        for col in range(n):
+            if is_safe(board, row, col):
+                board[row][col] = 'Q'
+                backtrack(row + 1)
+                board[row][col] = '.'
+    
+    backtrack(0)
+    
+    for solution in solutions:
+        for row in solution:
+            print(row)
+        print()
