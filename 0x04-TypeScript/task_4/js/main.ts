88a8cbfa5b2914625ca4723b46713f70520c8547
BelalGhonaim <belalpop1999@yahoo.com>
2024-05-27 22:32:03 +0300

    add files to typescript

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,20 @@
+export interface Student {
+    firstName: string,
+    lastName: string,
+    age: number,
+    location: string,
+    }
+
+    const student1: Student = {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 25,
+        location: 'Nairobi',
+    };
+
+    const student2: Student = {
+        firstName: 'Alex',
+        lastName: 'Sanchez',
+        age: 29,
+        location: 'Cairo',
+    };
